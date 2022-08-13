06112166fb8b998c60a3448bca1413104cb6cd08
Oumar Diallo <birusod@gmail.com>
2022-08-12 23:47:17 -0700

    temp

---
+++ mytemplate.R
@@ -0,0 +1,37 @@
+
+
+library(distill)
+library(postcards)
+
+
+create_post(
+  +     "Week-##",
+  +     collection = "posts",
+  +     author = "auto",
+  +     slug = "auto",
+  +     date = Sys.Date())
+
+
+
+## Setup & Libraries
+
+knitr::opts_chunk$set(echo = TRUE)
+library(tidyverse)
+library(scales)
+library(tidytuesdayR)
+library(rmarkdown)
+
+
+## Loading dataset
+
+### 
+
+## Exploring 
+
+## wrangling
+
+## Plotting
+
+#### {r, layout="l-body-outset", fig.width=8, fig.height=6}
+pp 
+pp
